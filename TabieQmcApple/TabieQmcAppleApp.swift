a49f9c7cc1476f922a570d91a7ddc8e44bf8e69b
shameem <shameem.jasir@gmail.com>
2024-08-04 10:26:26 +0300

    Initial Commit

---
+++ TabieQmcApple/TabieQmcAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabieQmcAppleApp.swift
+//  TabieQmcApple
+//
+//  Created by Shameem Jasir on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabieQmcAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
