40fc43dd7ed3dbb9c4eeb8d6374c0df96a777087
shameem <shameem.jasir@gmail.com>
2024-08-04 10:33:18 +0300

    Tvos project creation

---
+++ TabieQmcTvos/TabieQmcTvosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabieQmcTvosApp.swift
+//  TabieQmcTvos
+//
+//  Created by Shameem Jasir on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabieQmcTvosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
